6a9b7c8fce207fb94db2e77d9bcad8bd9767d23b
cvigueras <carlosj.vigueras@gmail.com>
2023-04-05 11:19:10 +0200

    Test: return_love_love_when_game_start

--- TennisScore.Test/GameShould.cs
+++ TennisScore.Test/GameShould.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace TennisScore.Test
 {
     public class GameShould
@@ -5,6 +7,24 @@ namespace TennisScore.Test
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void return_love_love_when_game_start()
+        {
+            var expectedResult = new List<string> {"love-love"};
+
+            var result = Game.GetScoreBoard();
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+
+    public class Game
+    {
+        public static object GetScoreBoard()
+        {
+            throw new NotImplementedException();
         }
     }
 }
