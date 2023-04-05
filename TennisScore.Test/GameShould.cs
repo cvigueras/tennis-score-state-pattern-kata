8feaddae8891d8b9d473147f2e484548fa87c8ea
cvigueras <carlosj.vigueras@gmail.com>
2023-04-05 11:23:25 +0200

    Test: return_fifteen_love_when_player1_win_first_point

--- TennisScore.Test/GameShould.cs
+++ TennisScore.Test/GameShould.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using System.Xml.Linq;
 using TennisScore.Console;
 
 namespace TennisScore.Test
@@ -16,6 +17,22 @@ namespace TennisScore.Test
             var expectedResult = new List<string> {"love-love"};
 
             var game = new Game();
+            var result = game.GetScoreBoard();
+
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void return_fifteen_love_when_player1_win_first_point()
+        {
+            var expectedResult = new List<string>{
+                {"love-love"},
+                {"fifteen-love"},
+            };
+
+            var game = new Game();
+            game.WinPoint("Player1");
+
             var result = game.GetScoreBoard();
 
             result.Should().BeEquivalentTo(expectedResult);
